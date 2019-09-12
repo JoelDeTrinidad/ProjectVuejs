1d3769d07bc24094dfbac53713b849e38871930f
Miguel Alejandro Lopez Detrinidad <miguel.lopez@codingtecs.com>
2019-09-11 18:12:08 -0600

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/ProjectVuejs/index.js'
-      : '/'
-  }
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/project-platzi/'
+    : '/'
+}
